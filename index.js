aaffe05826dda64a4592453ad6575faad177f885
Nikita Zavadskiy <nick.mr.volk@gmail.com>
2023-05-01 18:16:30 +0200

    add api module

---
+++ index.js
@@ -0,0 +1,132 @@
+import { getTodo, addTodo, deleteTodo } from "./api.js";
+
+// TODO: Получать из хранилища данных
+let tasks = [];
+
+let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+token = null;
+
+const fetchTodosAndRender = () => {
+	getTodo({ token })
+		.then((responseData) => {
+			tasks = responseData.todos;
+			renderApp();
+		});
+};
+
+const renderApp = () => {
+	const appEl = document.getElementById("app");
+
+	if (!token) {
+		const appHtml = `
+			<h1>Список задач</h1>
+					<div class="form">
+						<h3 class="form-title">Форма авторизации</h3>
+						<div class="form-row">
+							Логин
+							<input type="text" id="login-input" class="input" placeholder="User" />
+							Пароль
+							<br>
+							<input type="password" id="password-input" class="input" placeholder="Password" />
+						</div>
+						<br />
+						<button class="button" id="login-button">Войти</button>
+					</div>`
+		appEl.innerHTML = appHtml;
+
+		document.getElementById("login-button")
+			.addEventListener("click", () => {
+				token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+				fetchTodosAndRender();
+			})
+		return;
+	}
+
+	const tasksHtml = tasks
+		.map((task) => {
+			return `
+          <li class="task">
+            <p class="task-text">
+              ${task.text}
+              <button data-id="${task.id}" class="button delete-button">Удалить</button>
+            </p>
+          </li>`;
+		})
+		.join("");
+
+	const appHtml = `
+					<h1>Список задач</h1>
+					<ul class="tasks" id="list">
+						${tasksHtml}
+					</ul>
+					<br />
+					<div class="form">
+						<h3 class="form-title">Форма добавления</h3>
+						<div class="form-row">
+							Что нужно сделать:
+							<input type="text" id="text-input" class="input" placeholder="Выпить кофе" />
+						</div>
+						<br />
+						<button class="button" id="add-button">Добавить</button>
+					</div>`
+
+	appEl.innerHTML = appHtml;
+
+	const buttonElement = document.getElementById("add-button");
+	const listElement = document.getElementById("list");
+	const textInputElement = document.getElementById("text-input");
+
+	const deleteButtons = document.querySelectorAll(".delete-button");
+	for (const deleteButton of deleteButtons) {
+		deleteButton.addEventListener("click", (event) => {
+			event.stopPropagation();
+
+			const id = deleteButton.dataset.id;
+
+			// подписываемся на успешное завершение запроса с помощью then
+			deleteTodo({
+				token,
+				id,
+			})
+				.then((responseData) => {
+					// получили данные и рендерим их в приложении
+					tasks = responseData.todos;
+					renderApp();
+				});
+		});
+	}
+	buttonElement.addEventListener("click", () => {
+		if (textInputElement.value === "") {
+			return;
+		}
+
+		buttonElement.disabled = true;
+		buttonElement.textContent = "Задача добавляеятся...";
+
+		// подписываемся на успешное завершение запроса с помощью then
+		addTodo({
+			token,
+			text: textInputElement.value,
+		})
+			.then(() => {
+				// TODO: кинуть исключение
+				textInputElement.value = "";
+			})
+			.then(() => {
+				return fetchTodosAndRender();
+			})
+			.then(() => {
+				buttonElement.disabled = false;
+				buttonElement.textContent = "Добавить";
+			})
+			.catch((error) => {
+				console.error(error);
+				alert("Кажется у вас проблемы с интернетом, попробуйте позже");
+				buttonElement.disabled = false;
+				buttonElement.textContent = "Добавить";
+			});
+	});
+};
+
+// fetchTodosAndRender();
+renderApp();
