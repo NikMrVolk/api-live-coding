4b2800e77a55c710fd0b0b4e675d58a3ffaf7c7f
Nikita Zavadskiy <nick.mr.volk@gmail.com>
2023-05-01 20:54:39 +0200

    moved the login form to a separate component

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { getTodo, addTodo, deleteTodo } from "./api.js";
+import { renderLoginComponent } from "./components/login-component.js";
 
 // TODO: Получать из хранилища данных
 let tasks = [];
@@ -18,27 +19,13 @@ const renderApp = () => {
 	const appEl = document.getElementById("app");
 
 	if (!token) {
-		const appHtml = `
-			<h1>Список задач</h1>
-					<div class="form">
-						<h3 class="form-title">Форма авторизации</h3>
-						<div class="form-row">
-							Логин
-							<input type="text" id="login-input" class="input" placeholder="User" />
-							Пароль
-							<br>
-							<input type="password" id="password-input" class="input" placeholder="Password" />
-						</div>
-						<br />
-						<button class="button" id="login-button">Войти</button>
-					</div>`
-		appEl.innerHTML = appHtml;
-
-		document.getElementById("login-button")
-			.addEventListener("click", () => {
-				token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
-				fetchTodosAndRender();
-			})
+		renderLoginComponent({
+			appEl,
+			setToken: (newToken) => {
+				token = newToken;
+			},
+			fetchTodosAndRender,
+		});
 		return;
 	}
 
